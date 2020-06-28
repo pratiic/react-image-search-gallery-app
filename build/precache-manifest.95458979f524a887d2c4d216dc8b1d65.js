28e716bdf7bc1571f0e2de0ff4256b9f921bf3eb
pratik bhandari <pratikbhandari9292@gmail.com>
2020-06-28 17:50:26 +0545

    including the build directory

---
+++ build/precache-manifest.95458979f524a887d2c4d216dc8b1d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9871047119b101c3e9fbffb90af85f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "677582b3705a2c87a820",
+    "url": "/static/css/main.07b1fb21.chunk.css"
+  },
+  {
+    "revision": "9b175f87b664d667e164",
+    "url": "/static/js/2.fd23611e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd23611e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677582b3705a2c87a820",
+    "url": "/static/js/main.e86c36bb.chunk.js"
+  },
+  {
+    "revision": "cb1934056e2d0c2f3ea9",
+    "url": "/static/js/runtime-main.ec650a8c.js"
+  }
+]);
